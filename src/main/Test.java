a33a1bed2eecb34841bd82e9dc0552d603db9026
tusha <tusha@TUSHAR>
2025-08-12 18:59:58 +0530

    My second Commit

--- src/main/Test.java
+++ src/main/Test.java
@@ -7,6 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		TeamService service =new TeamService();
 		service.printTeamDetails();
+		System.out.println("Test GitHub");
 		
 		
 	}
